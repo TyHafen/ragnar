a696a5e9ae7242996b5ca7d913bab467586b57f9
TyHafen <tyhafen@gmail.com>
2022-07-07 19:04:17 -0600

    COmmit

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,49 +1,53 @@
 <template>
   <div class=" m-3 align-items-center justify-content-center">
-    <h1>Hello</h1>
-    <form>
-      <div class="form-group">
-        <label for="">Runner 1 pace</label>
-        <input type="number" class="form-control" v-model="runner1" aria-describedby="" placeholder="">
-        <small id="" class="form-text "></small>
-      </div>
-      <div class="form-group">
-        <label for="">Runner 2 pace</label>
-        <input type="number" v-model="runner2" class="form-control" placeholder="">
-      </div>
-      <div class="form-group">
-        <label for="">Runner 3 pace</label>
-        <input type="number" v-model="runner3" class="form-control" placeholder="">
-      </div>
-      <div class="form-group">
-        <label for="">Runner 4 pace</label>
-        <input type="number" class="form-control" v-model="runner4" aria-describedby="" placeholder="">
-        <small id="" class="form-text "></small>
-      </div>
-      <div class="form-group">
-        <label for="">Runner 5 pace</label>
-        <input type="number" v-model="runner5" class="form-control" placeholder="">
-      </div>
-      <div class="form-group">
-        <label for="">Runner 6 pace</label>
-        <input type="number" v-model="runner6" class="form-control" placeholder="">
-      </div>
-      <div class="form-group">
-        <label for="">Runner 7 pace</label>
-        <input type="number" v-model="runner7" class="form-control" placeholder="">
-      </div>
-      <div class="form-group">
-        <label for="">Runner 8 pace</label>
-        <input type="number" v-model="runner8" class="form-control" placeholder="">
-      </div>
-
-
-
-      <button type="submit" class="btn btn-primary m-2" @click="calculateTime()">Submit</button>
-    </form>
     <div class="row">
-      <h2>It will take this many hours to complete</h2>
+      <div class="col-md-4 m-3  d-flex justify-content-center">
+        <form>
+          <div class="form-group">
+            <label for="">Runner 1 pace</label>
+            <input type="number" class="form-control" v-model="runner1" aria-describedby="" placeholder="">
+            <small id="" class="form-text "></small>
+          </div>
+          <div class="form-group">
+            <label for="">Runner 2 pace</label>
+            <input type="number" v-model="runner2" class="form-control" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="">Runner 3 pace</label>
+            <input type="number" v-model="runner3" class="form-control" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="">Runner 4 pace</label>
+            <input type="number" class="form-control" v-model="runner4" aria-describedby="" placeholder="">
+            <small id="" class="form-text "></small>
+          </div>
+          <div class="form-group">
+            <label for="">Runner 5 pace</label>
+            <input type="number" v-model="runner5" class="form-control" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="">Runner 6 pace</label>
+            <input type="number" v-model="runner6" class="form-control" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="">Runner 7 pace</label>
+            <input type="number" v-model="runner7" class="form-control" placeholder="">
+          </div>
+          <div class="form-group">
+            <label for="">Runner 8 pace</label>
+            <input type="number" v-model="runner8" class="form-control" placeholder="">
+          </div>
+
+
+
+          <button type="submit" class="btn btn-primary m-2" @click="calculateTime()">Submit</button>
+        </form>
+      </div>
+      <div class="col-md-4 m-3 d-flex justify-content-center">
+        <h2>It will take this many hours to complete</h2>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -79,9 +83,9 @@ export default {
         console.log(AppState.runners)
         let totalTime = 0
 
-      for (let i = 0; i < runners.length, i++;) {
+        for (let i = 0; i < runners.length, i++;) {
          totalTime+= (runners[i]*15.1)
-      } console.log(totalTime)
+       } console.log(totalTime)
   }
 
 }
